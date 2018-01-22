c6dd2cd57883f9e0b01cd4fffa7e7999af0a9e43
Julia Jaspers <juliajaspers@Julias-MacBook-Pro.local>
2018-01-22 10:15:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+const express = require('express')
+const volleyball = require('volleyball')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(volleyball)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, './public')))
+
+app.listen(port)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
