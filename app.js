c5a20594cd68b951883d243e1f578f12d985daa0
Julia Jaspers <juliajaspers@Julias-MacBook-Pro.local>
2018-01-22 12:22:39 -0500

    added more squares

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ app.use(volleyball)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use(express.static(path.join(__dirname, '/components'))
+)
 
 app.listen(port)
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
